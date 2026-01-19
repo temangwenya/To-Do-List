341b7901a599597e5ed52d8fe2164186b702762d
Tema Ngwenya <ngwenyatema@gmail.com>
2026-01-19 04:34:09 +0200

    ArrayList Loop: - Added a loop to store tasks into an ArrayList (tasks) and display the stored tasks at the end. - New variable (task). - Task display moved to main, beacuse it must show at the end regardless of the selected menu item.

--- ToDoList2.java
+++ ToDoList2.java
@@ -7,7 +7,7 @@ public class ToDoList2 {
         // Global 
             //Objects
             static Scanner scanner = new Scanner(System.in);
-            static ArrayList<String> toDoList = new ArrayList<>();
+            static ArrayList<String> tasks = new ArrayList<>();
             //Variables
     
     public static void main(String[] args){
@@ -34,6 +34,13 @@ public class ToDoList2 {
                     System.out.println("Please input 1 or 2.");
                     //restart programme
             }
+        
+        // Final Display
+            System.out.println("_________________________");
+            System.out.println("Tasks for the day:"); // I want it to be: for <dqy>, <date>. To fetch it from addTask method, I probably need to make it global or return it, or pointers or something/
+            for (String t : tasks) {
+                System.out.println("- " + t);
+            }
         }
 
 
@@ -52,25 +59,23 @@ public class ToDoList2 {
             System.out.println("-----");
         
         // Tasks
+            String task = " ";
             String yesORno = " ";
-            
+                //loop collects inputs and stores them in an ArrayList
             do {
                 System.out.print("Task: ");
-                String task = scanner.nextLine();
+                task = scanner.nextLine();
                 System.out.println(" ");
+                tasks.add(task); // Add input to the ArrayList
                 
                 System.out.println("Wanna add another task? (yes/no)");
                 yesORno = scanner.nextLine();
                 System.out.println(" ");
                 if (yesORno == "no"){
+                    scanner.close(); //cosing the scanner is good practice. Not sure why though.
                     break;
                 }
-            } while (yesORno.equals("yes"));
-                
-                
-            System.out.println("_________________________");
-            System.out.println("Tasks for " + day_ + ", " + date + ":"); 
-            
+            } while (yesORno.equals("yes")); 
     }    
 
     static void checkOffTasks() {
